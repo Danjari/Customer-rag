5772dab5d523fc076189fd729b8851e310e754a8
Boubacar Ballo <73037697+boubalkaly@users.noreply.github.com>
2024-08-08 22:31:31 +0000

    Add router

---
+++ app/api/openai/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+export async function POST(request) {
+  const openai = new OpenAI({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  });
+  const data = await resquest.json();
+  const completiton = openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: systemPrompt,
+      },
+      ...data,
+    ],
+    model: "gpt-4o-mini",
+    stream: true,
+  });
+}
